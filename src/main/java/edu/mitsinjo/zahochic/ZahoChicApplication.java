d903391a310dd95dd20c4f271e67b0beef12ecc5
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2025-03-29 12:03:10 +0300

    Initial commit

---
+++ src/main/java/edu/mitsinjo/zahochic/ZahoChicApplication.java
@@ -0,0 +1,13 @@
+package edu.mitsinjo.zahochic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZahoChicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZahoChicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mitsinjo/zahochic/ZahoChicApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mitsinjo.zahochic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZahoChicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
